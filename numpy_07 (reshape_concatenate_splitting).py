09ae27f1f9dff0404a7a37e036297fe7e52f205c
AvinashSubhash <59025615+AvinashSubhash@users.noreply.github.com>
2020-03-19 18:04:28 +0530

    Update numpy_07 (reshape_concatenate_splitting).py

--- numpy_07 (reshape_concatenate_splitting).py
+++ numpy_07 (reshape_concatenate_splitting).py
@@ -8,7 +8,8 @@ Created on Tue Mar 17 22:38:32 2020
 import numpy as np
 
 array1=np.full((3,4),3)
-array1=array1.reshape((3,4))
+array1=array1.reshape((4,3))
+
 
 
 print(array1[:,np.newaxis])
@@ -18,8 +19,22 @@ array2=np.ones((3,4))
 print(array2)
 
 
-np.concatenate([array1,array2],axis=0)
+array2=np.ones((3,4))
 
+"""Horizontally stacking up the array"""
+array3=np.hstack([array1,array2])
+print(array3)
+
+
+"""splitting of arrays and matrix"""
+
+array4=np.arange(16)
+array4=array4.reshape((4,4))
+print(array4)
+
+print(np.vsplit(array4,[1,2,3]),"\n")
+
+print(np.hsplit(array4,[1,2,3]),"\n")
 
 
 
