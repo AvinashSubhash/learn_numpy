958c07a22423f34d7e41fbc7fe260361ae2aaae4
Parmodsihag <68704187+Parmodsihag@users.noreply.github.com>
2020-09-30 21:27:30 +0530

    added some comments more about array

--- numpy_01 (testing_with_single_dimension).py
+++ numpy_01 (testing_with_single_dimension).py
@@ -7,8 +7,20 @@ Created on Sun Feb  15 14:30:35 2020
 
 import numpy as np
 vowels="aeiou"
-a=np.array(["avinash","akash"])
+a=np.array(["avinash","akash"]) # it creates a 1d array
 for i in a:
     for j in i:
         if j in vowels:
             print(j,": in element ",i,"\n")
+            
+''' 
+>>> a
+array(['avinash', 'akash'], dtype='<U7')
+
+>>> print(a)
+['avinash' 'akash']
+
+>>> type(a)
+<class 'numpy.ndarray'>
+
+'''
